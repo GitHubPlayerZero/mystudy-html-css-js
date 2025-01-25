4816699c36ee0b1ed06b60c0a4d05edf89a2a55c
GitHubPlayer0 <65072506+GitHubPlayerZero@users.noreply.github.com>
2025-01-25 11:24:19 +0800

    增加模組範例

---
+++ practice/module/module.js
@@ -0,0 +1,23 @@
+console.log(`==========`);
+// 取得模組 URL 資訊
+const moduleUrl = new URL(import.meta.url);
+console.log(`Module URL ==>`, moduleUrl);
+
+// 取得模組 name 參數
+let name = moduleUrl.searchParams.get("name");
+name = name ? `[${name}] ` : "";
+console.log(`${name}我是模組的 JS`);
+
+let cnt = 0;
+console.log(`${name}cnt ==> ${cnt}`);
+cnt ++;
+console.log(`${name}cnt ==> ${cnt}`);
+
+export function getCnt() {
+  return cnt;
+}
+
+export function addCnt () {
+  cnt ++;
+  console.log(`${name}addCnt ==> ${cnt}`);
+}
